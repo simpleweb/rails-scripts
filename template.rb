a5a4298ff623698c9031c65c18293bb1570387bb
Chris Mytton <self@hecticjeff.net>
2013-02-01 15:51:00 +0000

    Add Simpleweb Rails template

---
+++ template.rb
@@ -0,0 +1,43 @@
+run 'rm README.rdoc'
+run 'rm public/index.html'
+run 'rm -rf test'
+
+gem 'airbrake'
+gem 'capistrano'
+gem 'rack-git_sha'
+gem 'lograge'
+
+gem_group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'meta_request'
+end
+
+gem_group :development, :test do
+  gem 'rspec-rails'
+end
+
+gem_group :test do
+  gem 'shoulda-matchers'
+end
+
+run "bundle install --quiet"
+
+generate('rspec:install')
+
+environment 'config.lograge.enabled = true', env: :production
+
+application do
+  <<-GENERATORS
+config.app_generators do |g|
+      g.test_framework :rspec, views: false, view_specs: false, helpers: false, helper_specs: false, routing_specs: false
+      g.stylesheets false
+      g.javascripts false
+    end
+  GENERATORS
+end
+
+route "mount Rack::GitSha => '/sha'"
+
+git :init
+git add: '.'
